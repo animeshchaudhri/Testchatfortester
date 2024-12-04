c519242f87a4e842c761821620a61aac44c7c3d2
animesh chaudhri <111721851+animeshchaudhri@users.noreply.github.com>
2024-12-04 19:41:21 +0530

    Feat: Video call

--- client/src/app/(home)/components/body/Header.tsx
+++ client/src/app/(home)/components/body/Header.tsx
@@ -42,7 +42,7 @@ const Header: React.FC<HeaderProps> = ({ conversation, chatType }) => {
         isOpen={drawerOpen}
         onClose={() => setDrawerOpen(false)}
       />
-      {/* <VideoCall data={conversation} /> */}
+  <VideoCall data={conversation} /> 
       <div className="bg-white w-full flex border-b-[1px] sm:px-4 py-4 px-4 lg:px-6 justify-between items-center shadow-sm">
         <div className="flex gap-3 items-center">
           <Link
